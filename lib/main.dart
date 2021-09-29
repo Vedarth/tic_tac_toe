dbf4ec986109814a929845b79e6a4d216a980e71
Vedarth <vedarth.sharma@gmail.com>
2021-09-29 20:00:52 +0530

    Add main logic and ui elements

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+        appBarTheme: AppBarTheme(centerTitle: true)),
       home: MyHomePage(title: title),
     );
   }
@@ -34,7 +34,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _numberOfCells = 3;
   double _size = 32;
-  double _gridSize = 114;
+  double _gridSize = 90;
   int _xScore = 0;
   int _oScore = 0;
 
@@ -53,49 +53,57 @@ class _MyHomePageState extends State<MyHomePage> {
         (_) => List.generate(_numberOfCells, (_) => Cell.none),
       ));
 
+  Color _getBackgroundColor() {
+    final thisMove = lastMove == Cell.X ? Cell.O : Cell.X;
+
+    return _getCellColor(thisMove).withAlpha(150);
+  }
+
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: _getBackgroundColor(),
         appBar: AppBar(
           title: Text(widget.title),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: getChildren(),
+          children: _getChildren(),
         ),
       );
 
-  List<Widget> getChildren() {
+  List<Widget> _getChildren() {
     List<Widget> children = <Widget>[
       Container(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            getScoreView("Player X", _xScore, Colors.red, _size),
-            getScoreView("Player O", _oScore, Colors.blue, _size)
+            _getScoreView("Player X", _xScore, Colors.red, _size),
+            _getScoreView("Player O", _oScore, Colors.blue, _size)
           ],
         ),
       ),
     ];
-    children.addAll(Utils.modelBuilder(grid, (x, value) => buildRow(x)));
+    children.addAll(Utils.modelBuilder(grid, (x, value) => _buildRow(x)));
 
     return children;
   }
 
-  Widget buildRow(int x) {
+  Widget _buildRow(int x) {
     final values = grid[x];
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: Utils.modelBuilder(
         values,
-        (y, value) => buildField(x, y),
+        (y, value) => _buildCell(x, y),
       ),
     );
   }
 
-  Widget buildField(int x, int y) {
+  
+
+  Widget _buildCell(int x, int y) {
     final value = grid[x][y];
-    final color = getFieldColor(value);
+    final color = _getCellColor(value);
 
     return Container(
       margin: EdgeInsets.all(4),
@@ -104,13 +112,56 @@ class _MyHomePageState extends State<MyHomePage> {
           minimumSize: Size(_gridSize, _gridSize),
           primary: color,
         ),
-        child: Text(value, style: TextStyle(fontSize: 32)),
-        onPressed: () => _setEmptyCells(),
+        child: Text(value, style: TextStyle(fontSize: _size)),
+        onPressed: () => _selectField(value, x, y),
       ),
     );
   }
 
-  Color getFieldColor(String value) {
+  void _selectField(String value, int x, int y) {
+    if (value == Cell.none) {
+      final newValue = lastMove == Cell.X ? Cell.O : Cell.X;
+
+      setState(() {
+        lastMove = newValue;
+        grid[x][y] = newValue;
+      });
+
+      if (_isWinner(x, y)) {
+        if (newValue == Cell.X) {
+          _xScore += 1;
+        } else {
+          _oScore += 1;
+        }
+        _showEndDialog('Player $newValue Won');
+      } else if (_isEnd()) {
+        _showEndDialog('Undecided Game');
+        setState(() {
+          lastMove = Cell.none;
+        });
+      }
+    }
+  }
+
+  bool _isEnd() =>
+      grid.every((values) => values.every((value) => value != Cell.none));
+
+  bool _isWinner(int x, int y) {
+    var col = 0, row = 0, diag = 0, rdiag = 0;
+    final player = grid[x][y];
+    final n = _numberOfCells;
+
+    for (int i = 0; i < n; i++) {
+      if (grid[x][i] == player) col++;
+      if (grid[i][y] == player) row++;
+      if (grid[i][i] == player) diag++;
+      if (grid[i][n - i - 1] == player) rdiag++;
+    }
+
+    return row == n || col == n || diag == n || rdiag == n;
+  }
+
+  Color _getCellColor(String value) {
     switch (value) {
       case Cell.O:
         return Colors.blue;
@@ -121,7 +172,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Padding getScoreView(String player, int score, MaterialColor color, double size) {
+  Padding _getScoreView(
+      String player, int score, MaterialColor color, double size) {
     return Padding(
       padding: const EdgeInsets.all(30.0),
       child: Column(
@@ -134,11 +186,56 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Text(
             score.toString(),
-            style: TextStyle(fontSize: 32, color: color),
+            style: TextStyle(fontSize: size, color: color),
           ),
         ],
       ),
     );
   }
+
+  void _clearScoreBoard() {
+    _setEmptyCells();
+    setState(() {
+      _xScore = 0;
+      _oScore = 0;
+    });
+  }
+
+  Color _getEndGameColor(String value) {
+    switch (value) {
+      case Cell.O:
+        return Colors.blue;
+      case Cell.X:
+        return Colors.red;
+      default:
+        return Colors.black;
+    }
+  }
+
+  void _showEndDialog(String title) => showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+          title:
+              Text(title, style: TextStyle(color: _getEndGameColor(lastMove))),
+          content: Text('Press to Restart the Game'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                _clearScoreBoard();
+                Navigator.of(context).pop();
+              },
+              child: Text('Clear Score Board'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _setEmptyCells();
+                Navigator.of(context).pop();
+              },
+              child: Text('Restart'),
+            ),
+          ],
+        ),
+      );
 
 }
